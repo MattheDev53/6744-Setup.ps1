a4a919cecae6e66402b4e9904a84bda46d301d9d
MattheDev53 <matthew.c.albrecht@gmail.com>
2025-11-25 09:28:49 -0600

    Do stuff. (Committing for later use)

--- main.ps1
+++ main.ps1
@@ -1,19 +1,107 @@
+function Get-BrowserPreference {
+	<#
+.DESCRIPTION
+Get-BrowserPreference gets the user's prefered browser
+
+.OUTPUTS
+Outputs the WinGet ID of the browser selected (Defaults to Edge)
+	#>
+	$choices = @(
+			(New-Object System.Management.Automation.Host.ChoiceDescription(
+				"&Edge",
+				"No new browser is installed"
+			)),
+			(New-Object System.Management.Automation.Host.ChoiceDescription(
+				"&Zen Browser",
+				"Installs Zen Browser"
+			))
+			(New-Object System.Management.Automation.Host.ChoiceDescription(
+				"&Firefox",
+				"Installs Firefox"
+			))
+			(New-Object System.Management.Automation.Host.ChoiceDescription(
+				"Chrom&ium",
+				"Installs Chromium"
+			))
+			(New-Object System.Management.Automation.Host.ChoiceDescription(
+				"&Chrome",
+				"Installs Chrome"
+			))
+	)
+	$choice = $host.ui.PromptForChoice("Browser Selection","Select a Browser",$choices,0)
+	switch ( $choice ) {
+		0 {return "Microsoft.Edge"}
+		1 {return "Zen-Team.Zen-Browser"}
+		2 {return "Mozilla.Firefox"}
+		3 {return "Hibbiki.Chromium"}
+		4 {return "Google.Chrome"}
+	}
+}
+
 # Make sure the directory is consistent
 Set-Location ~/
 
+Write-Output @"
+   _____      ___    ____            _____
+  / ___/___  /   |  / __ \_________ / ___/
+  \__ \/ _ \/ /| | / /_/ / ___/ __ \\__ \ 
+ ___/ /  __/ ___ |/ ____/ /  / /_/ /__/ / 
+/____/\___/_/  |_/_/   /_/   \____/____/  
+
+[Se]mi
+[A]utonomous
+[Pro]visioning
+[S]ystem
+
+Built by MattheDev53 <matthew.c.albrecht@gmail.com>
+To be Licensed under AGPL-3.0
+"@
+
+$tasks = 2
+$doingTask = 0
+
+$doingTask++
+Write-Output "Task $doingTask/$tasks: Installing JetBrains Mono"
+
+Write-Output "[Task #$doingTask] Downloading JetBrains Mono"
 Invoke-WebRequest "https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip" -OutFile font.zip
+
+Write-Output "[Task #$doingTask] Unzipping the font files"
 Expand-Archive .\font.zip
+
+Write-Output "[Task #$doingTask] Deleting the original zip file"
 Remove-Item font.zip # Clean up the zip file
+
+Write-Output "[Task #$doingTask] Installing Fonts"
 Get-ChildItem -Path .\font\fonts\ttf | ForEach-Object {
     Write-Output "Installing $_"
     Copy-Item $_ "C:\Windows\Fonts"
   
     #register font for all users
     New-ItemProperty -Name $_.BaseName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $_.name}
+Write-Output "[Task #$doingTask] Removing original font files"
 Remove-Item .\font -Recurse -Force
 
+$doingTask++
+Write-Output "Task $doingTask/$tasks: Installing Posy's Improved Cursors"
+
+Write-Output "[Task #$doingTask] Downloading Posy's Improved Cursors"
 Invoke-WebRequest "https://www.michieldb.nl/other/cursors/Posy's%20Cursor%20Black.zip" -OutFile mouse.zip
+
+Write-Output "[Task #$doingTask] Unzipping the cursor files"
 Expand-Archive .\mouse.zip
+
+Write-Output "[Task #$doingTask] Deleting the original zip file"
 Remove-Item .\mouse.zip
-Start-Process ".\mouse\Posy's Cursor Black\_install Posy Black.inf" -Verb Install
+
+Write-Output "[Task #$doingTask] Running cursor installation file"
+Start-Process ".\mouse\Posy's Cursor Black\_install Posy Black.inf" -Verb Install -Wait
+
+Write-Output "[Task #$doingTask] Removing cursor files"
+Remove-Item .\mouse -Recurse -Force
+
+$doingTask++
+Write-Output "Task $doingTask/$tasks: Installing Apps"
+
+# Write-Output "[Task #$doingTask] ..."
 
--- testing.ps1
+++ testing.ps1
@@ -1,36 +1,27 @@
-function Get-BrowserPreference {
-	$choices = @(
-			(New-Object System.Management.Automation.Host.ChoiceDescription(
-				"&Edge",
-				"No new browser is installed"
-			)),
-			(New-Object System.Management.Automation.Host.ChoiceDescription(
-				"&Zen Browser",
-				"Installs Zen Browser"
-			))
-			(New-Object System.Management.Automation.Host.ChoiceDescription(
-				"&Firefox",
-				"Installs Firefox"
-			))
-			(New-Object System.Management.Automation.Host.ChoiceDescription(
-				"Chrom&ium",
-				"Installs Chromium"
-			))
-			(New-Object System.Management.Automation.Host.ChoiceDescription(
-				"&Chrome",
-				"Installs Chrome"
-			))
-	)
-	$choice = $host.ui.PromptForChoice("Browser Selection","Select a Browser",$choices,0)
-	switch ( $choice ) {
-		0 {return "Microsoft.Edge"}
-		1 {return "Zen-Team.Zen-Browser"}
-		2 {return "Mozilla.Firefox"}
-		3 {return "Hibbiki.Chromium"}
-		4 {return "Google.Chrome"}
-	}
-}
+Write-Output @"
+   _____      ___    ____            _____
+  / ___/___  /   |  / __ \_________ / ___/
+  \__ \/ _ \/ /| | / /_/ / ___/ __ \\__ \ 
+ ___/ /  __/ ___ |/ ____/ /  / /_/ /__/ / 
+/____/\___/_/  |_/_/   /_/   \____/____/  
 
+[Se]mi
+[A]utonomous
+[Pro]visioning
+[S]ystem
+
+Built by MattheDev53 <matthew.c.albrecht@gmail.com>
+To be Licensed under AGPL-3.0
+"@
+
+Write-Output "! [Notice] !"
+Write-Output "|> You are running testing.ps1 as opposed to main.ps1, meaning this will     "
+Write-Output "|> likely not install any new software or change system settings. On the off "
+Write-Output "|> chance this file does something, please press <Ctrl+C> to terminate the   "
+Write-Output "|> program before it starts making unexpected changes to your system. If you "
+Write-Output "|> can read Powershell, have read this program, and know what it does, be my "
+Write-Output "|> guest, but don't say I didn't warn you.                                   "
+# Write-Output "|>                                                                           "
 function Get-VSCodePreference {
 	$choices = @(
 			(New-Object System.Management.Automation.Host.ChoiceDescription(
@@ -46,10 +37,58 @@ function Get-VSCodePreference {
 				"Telemetry free build of VSCode"
 			))
 	)
-	$choice = $host.ui.PromptForChoice("Browser Selection","Select a Browser",$choices,0)
+	$choice = $host.ui.PromptForChoice("VSCode Version Selection","Select a Version",$choices,0)
 	switch ( $choice ) {
 		0 {return "VSCode"}
 		1 {return "VSCode Insider"}
 		2 {return "VSCodium"}
 	}
 }
+
+function Inquire-LibraryDrive {
+	$choices = @(
+			(New-Object System.Management.Automation.Host.ChoiceDescription(
+				"&Yes",
+				"You have a WPILib ISO on D:\ that is named properly (faster)"
+			)),
+			(New-Object System.Management.Automation.Host.ChoiceDescription(
+				"&No",
+				"You do not have a WPILib ISO on D:\ or you want to install from the internet (slower)"
+			))
+	)
+	$choice = $host.ui.PromptForChoice(
+		"SeAProS Drive Query : WPILib",
+		"Do you have a WPILib ISO on D:\ named ``wpi.iso``?",
+		$choices,1
+	)
+	switch ( $choice ) {
+		0 {return "Install from drive"}
+		1 {return "Install from internet"}
+	}
+}
+function Inquire-ToolsDrive {
+	$choices = @(
+			(New-Object System.Management.Automation.Host.ChoiceDescription(
+				"&Yes",
+				"You have the NI Game Tools Installer on D:\ that is named properly (faster)"
+			)),
+			(New-Object System.Management.Automation.Host.ChoiceDescription(
+				"&No",
+				"You do not have the NI Game Tools Installer on D:\ or you want to install from the internet (slower)"
+			))
+	)
+	$choice = $host.ui.PromptForChoice(
+		"SeAProS Drive Query : Game Tools",
+		"Do you have the NI Game Tools Installer on D:\ named ``ni.exe``?",
+		$choices,1
+	)
+	switch ( $choice ) {
+		0 {return "Install from drive"}
+		1 {return "Install from internet"}
+	}
+}
+
+$foo = Inquire-LibraryDrive
+Write-Output $foo
+$bar = Inquire-ToolsDrive
+Write-Output $bar
