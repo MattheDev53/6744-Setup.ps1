8c92f54cbff474a216d7d9350eabb94c24706a78
MattheDev53 <110939089+MattheDev53@users.noreply.github.com>
2025-11-26 19:40:08 -0600

    It's in

--- modules/packages.ps1
+++ modules/packages.ps1
@@ -2,8 +2,8 @@ function Install-Packages {
 	$packages = @(
 		"REVRobotics.REVHardwareClient",
 		"9NVV4PWDW27Z", # Phoenix Tuner X (From MS Store)
-		"9NQBKB5DW909"  # Pathplanner     (From MS Store)
-		# "grapplerobotics.GrappleHook" # Not on winget yet
+		"9NQBKB5DW909",  # Pathplanner     (From MS Store)
+		"grapplerobotics.GrappleHook" # Not on winget yet
 	)
 	$packages | ForEach-Object {
 		Write-Output "Installing $_"
